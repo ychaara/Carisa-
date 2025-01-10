55314c204a115a524f433ee257d4b88711a24b5f
Ycaa <167733926+ychaara@users.noreply.github.com>
2025-01-10 07:48:43 +0700

    Add files via upload

--- dashboard.dart
+++ dashboard.dart
@@ -15,27 +15,11 @@ class _Dashboard_PageState extends State<Dashboard_Page> {
   bool voucherAktif = false;
   bool isVisible = true;
   bool isvisible = true;
-  //counter (untuk angka pemesanan)
+  // Counter (untuk angka pemesanan)
   int counter1 = 0;
   int counter2 = 0;
   int counter3 = 0;
   int counter4 = 0;
-  void incrementCounters() {
-    setState(() {
-      counter1++;
-      counter2++;
-      counter3++;
-      counter4++;
-    });
-  }
-
-  int get totalPesanana => counter1 + counter2 + counter3 + counter4;
-
-  @override
-  void dispose() {
-    voucherController.dispose();
-    super.dispose();
-  }
 
   int total = 0;
 
@@ -46,11 +30,16 @@ class _Dashboard_PageState extends State<Dashboard_Page> {
 
   int diskon = 0;
 
-  int potongan(diskon, total) {
+  @override
+  void dispose() {
+    voucherController.dispose();
+    super.dispose();
+  }
+
+  int potongan(int diskon, int total) {
     var c = diskon / 100;
     var d = total * c;
-    var e = total - d;
-    return e.toInt();
+    return total - d.toInt();
   }
 
   @override
@@ -108,6 +97,111 @@ class _Dashboard_PageState extends State<Dashboard_Page> {
             ),
           ),
         ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView(
+              children: [
+                Container(
+                  margin: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 5,
+                  ),
+                  height: 100,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.white,
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.grey,
+                        blurRadius: 2,
+                        offset: Offset(0, 0),
+                        spreadRadius: 1,
+                      ),
+                    ],
+                  ),
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.symmetric(
+                          horizontal: 10,
+                          vertical: 10,
+                        ),
+                        height: 50,
+                        width: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: const DecorationImage(
+                            image: AssetImage('assets/Menu1.png'),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(top: 12),
+                              child: Text(
+                                'Cappucino',
+                                style: GoogleFonts.poppins(
+                                  fontSize: 13,
+                                  fontWeight: FontWeight.w500,
+                                  color: const Color(0xFFA4A4A4),
+                                ),
+                              ),
+                            ),
+                            Text(
+                              'Rp $harga1',
+                              style: GoogleFonts.poppins(
+                                fontSize: 13,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.brown,
+                              ),
+                            ),
+                            const Spacer(),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                IconButton(
+                                  icon: const Icon(Icons.remove),
+                                  onPressed: () {
+                                    if (counter1 > 0) {
+                                      setState(() {
+                                        counter1 -= 1;
+                                        total -= harga1;
+                                      });
+                                    }
+                                  },
+                                ),
+                                Text(
+                                  '$counter1',
+                                  style: const TextStyle(fontSize: 16),
+                                ),
+                                IconButton(
+                                  icon: const Icon(Icons.add),
+                                  onPressed: () {
+                                    setState(() {
+                                      counter1 += 1;
+                                      total += harga1;
+                                    });
+                                  },
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
